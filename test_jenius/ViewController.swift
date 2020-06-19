e9e834632ac84573691db90fd1b8cb1bcf5e5ffc
Bimo <satriantorobimo@gmail.com>
2020-06-19 10:28:40 +0700

    Initial Commit

---
+++ test_jenius/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_jenius
+//
+//  Created by Bimo Satriantoro on 19/06/20.
+//  Copyright © 2020 Bimo Satriantoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
